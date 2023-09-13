ef68ffd0201339dde89452587f6953ea90991e4a
nitishjaiswal2002 <141999440+nitishjaiswal2002@users.noreply.github.com>
2023-09-13 16:02:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ function decimalToBinary(num) {
   //Write you code here
 let binary = ""
   while(num>0){
-    binary = num%2 + binary
-    num=Math.floor(num/2)
+    binary = num%2 + binary;
+    num=Math.floor(num/2);
   }
-  return binary
+  return binary;
 }
 
 
